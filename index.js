35acdec4ab9eee7b150c307581094bc8f0d9a862
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-28 17:44:08 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-function callMe() {
-  console.log("Hello, world!");
+function callMe(name) {
+  return `Hello, ${name}!`;
 }
+
+const dontCall = (name) => {
+  console.log(`Hello, ${name}!`);
+};
 
 const square = (number) => number * number;
 
-console.log(square(2), callMe());
+console.log(square(2));
+console.log(callMe("Bruh"));
+dontCall("John");
